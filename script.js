5f442172ee49d1e49896ce567e5063c91a262b94
ChoforJr <choforforsakang@gmail.com>
2024-06-06 23:56:58 +0100

    Adding logic for the round by round play
    Since there is a lot of condition I use switch statements for their
    readability and I compare the input of the user with the computer choice
    to ge the result

--- script.js
+++ script.js
@@ -26,3 +26,36 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    switch (humanChoice,computerChoice){
+        case humanChoice=='rock'&&computerChoice=='paper':
+            computerScore+=1;
+            return console.log('You lose! Paper beats Rock');
+        case humanChoice=='paper'&&computerChoice=='scissors':
+            computerScore+=1;
+            return console.log('You lose! Scissors beats Paper');
+        case humanChoice=='scissors'&&computerChoice=='rock':
+            computerScore+=1;
+            return console.log('You lose! Rock beats Scissors');
+        case humanChoice=='paper'&&computerChoice=='rock':
+            humanScore+=1;
+            return console.log('You win! Paper beats Rock');
+        case humanChoice=='scissors'&&computerChoice=='paper':
+            humanScore+=1;
+            return console.log('You win! Scissors beats Paper');
+        case humanChoice=='rock'&&computerChoice=='scissors':
+            humanScore+=1;
+            return console.log('You win! Rock beats Scissors');
+        default :
+        return console.log('It is a Draw!');
+    }
+   
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+  
+
