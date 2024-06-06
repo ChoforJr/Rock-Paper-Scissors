09d10c998e3349a0f995c4642f1a9e6178cb0e6c
ChoforJr <choforforsakang@gmail.com>
2024-06-06 23:04:59 +0100

    Added the function for Human Choice
    Used prompt to receive human input and and verified if the input is
    valid by comparing if to the answer and console logging back, also
    added variables for score counter

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random()*100);
     if (computerChoice <=30) {
-        return console.log('Rock');
+        return console.log('rock');
     } else if (computerChoice>30 && computerChoice<=60) {
-        return console.log('Paper');
+        return console.log('paper');
     }else {
-        return console.log('Scissors');
+        return console.log('scissors');
     }
 }
-getComputerChoice();
+
+function getHumanChoice () {
+    let humanChoice = prompt('Input rock, paper or scissors', 'paper');
+    if (humanChoice == 'rock'){
+        return console.log('rock');
+    }else if (humanChoice == 'paper'){
+        return console.log('paper');
+    }else if (humanChoice == 'scissors') {
+        return console.log('scissors');
+    }else {
+        return console.log('invalid input');
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
